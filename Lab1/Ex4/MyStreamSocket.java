593be8da80c0cb2e55d838797b475201eefe7d69
Yunnna005 <annakovalenko0500@gmail.com>
2026-01-28 09:53:46 +0000

    create Folder Ex4; create java class MyStreamSocket with code from the Lab 1 Socket Programming 2024.docx file

---
+++ Lab1/Ex4/MyStreamSocket.java
@@ -0,0 +1,54 @@
+package mtu_distributed_computing.Lab1.Ex4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class MyStreamSocket extends Socket {
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    MyStreamSocket(String acceptorHost,int acceptorPort ) throws SocketException,IOException{
+        socket = new Socket(acceptorHost, acceptorPort );
+        setStreams( );
+    }
+    
+    MyStreamSocket(Socket socket) throws IOException {
+        this.socket = socket;
+        setStreams( );
+    }
+
+    private void setStreams( ) throws IOException{
+        // get an input stream for reading from the data socket
+        InputStream inStream = socket.getInputStream();
+        input = new BufferedReader(new InputStreamReader(inStream));
+        OutputStream outStream = socket.getOutputStream();
+        // create a PrinterWriter object for character-mode output
+        output = new PrintWriter(new OutputStreamWriter(outStream));
+    }
+
+    public void sendMessage(String message) throws IOException {
+        output.print(message + "\n");
+        //The ensuing flush method call is necessary for the data to
+        // be written to the socket data stream before the
+        // socket is closed.
+        output.flush();
+    } // end sendMessage
+        
+    public String receiveMessage( ) throws IOException {
+        // read a line from the data stream
+        String message = input.readLine( );
+        return message;
+    } //end receiveMessage
+        
+    public void close( ) throws IOException {
+        socket.close( );
+    }
+}
