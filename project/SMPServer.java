cd81b028e76743503f4f9a489a015b59cf472613
Yunnna005 <annakovalenko0500@gmail.com>
2026-04-01 10:38:43 +0000

    fixed server

--- project/SMPServer.java
+++ project/SMPServer.java
@@ -1,8 +1,17 @@
+import java.io.FileInputStream;
 import java.net.*;
+import java.security.KeyStore;
 import java.util.*;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
 public class SMPServer {
-    private static List<String> messages = new ArrayList<String>();
+
+    static ArrayList<String> messageStored = new ArrayList<String>();
 
     public static void main(String[] args) {
         int serverPort = 7;
@@ -10,31 +19,54 @@ public class SMPServer {
             serverPort = Integer.parseInt(args[0]); 
         }
 
+        String keystoreName = "serverkeystore.jks";
+        String keystorePassword = "password";
+        String keyPassword = "password";
+
         try {
-            ServerSocket myConnectionSocket = new ServerSocket(serverPort);
-            System.out.println("Server ready on port " + serverPort); 
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(new FileInputStream(keystoreName), keystorePassword.toCharArray());
+            
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyManagerFactory.init(keyStore, keyPassword.toCharArray());
+            
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+            SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
+            SSLServerSocket myConnectionSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(serverPort);
+            System.out.println("Server ready on port " + serverPort + ". PLease wait for connections...");
 
             while (true) {
-                Socket myDataSocket = myConnectionSocket.accept();
-                System.out.println("Connection accepted from " + myDataSocket.getInetAddress());
-                Thread theThread = new Thread(new SMPServerThread(myDataSocket));
-                theThread.start();
+                SSLSocket clientSocket = (SSLSocket) myConnectionSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+                MyStreamSocket myStreamSocket = new MyStreamSocket(clientSocket);
+                Thread clientThread = new Thread(new SMPServerThread(myStreamSocket));
+                clientThread.start();
             }
         } catch (Exception ex) {
+            System.out.println("Error starting server: " + ex.getMessage());
             ex.printStackTrace();
         }
     }
 
-    public static synchronized void addMessage(String msg) {
-        messages.add(msg);
-    }
-
-    public static synchronized String getMessage(int id) {
-        if (id < 1 || id > messages.size()) return null;
-        return messages.get(id - 1);
+    public static synchronized void addMessage(String message) {
+        messageStored.add(message);
     }
 
     public static synchronized List<String> getAllMessages() {
-        return new ArrayList<>(messages);
+        return new ArrayList<String>(messageStored);
     }
+
+    public static synchronized String getMessage(int index) {
+        if (index >= 0 && index < messageStored.size()) {
+            return messageStored.get(index);
+        }
+        return null;
+    }
+
+    public static synchronized int getMessageCount() {
+      return messageStored.size();
+   }
+ 
 }
